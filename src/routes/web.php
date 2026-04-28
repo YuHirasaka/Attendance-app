5bcf0702a8f74f7a6d3e2418d3858c3aa580b3b7
yu_hirasaka <yu.hirasaka03@gmail.com>
2026-04-28 20:31:43 +0900

    feat: 認証機能と勤怠登録画面のルート設定を追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Requests\EmailVerificationRequest;
+use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
+use Laravel\Fortify\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\AttendanceController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +17,23 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth', 'verified'])->group(function (){
+    Route::get('/attendance', [AttendanceController::class, 'show'])->name('attendance.show');
 });
+
+Route::post('login', [AuthenticatedSessionController::class, 'store']);
+Route::post('/register', [RegisteredUserController::class, 'store'])->name('register.store');
+
+Route::get('/email/verify', function() {
+    return view('auth.verify-email');
+})->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+    return redirect()->route('mypage.profile');
+})->middleware(['signed'])->name('verification.verify');
+
+Route::post('email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+    return back()->with('status', 'verification-link-sent');
+})->middleware(['throttle:6,1'])->name('verification.send');
