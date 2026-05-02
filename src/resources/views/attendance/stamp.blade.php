2895e2534badf7d2fb12c59d38e75cd5284d18a4
yu_hirasaka <yu.hirasaka03@gmail.com>
2026-05-02 15:39:44 +0900

    feat: jsで日時情報取得機能を実装

--- src/resources/views/attendance/stamp.blade.php
+++ src/resources/views/attendance/stamp.blade.php
@@ -12,13 +12,33 @@
 <div class="attendance">
     <section class="attendance-card">
         <p class="attendance-card__status">勤務外</p>
-        <p class="attendance-card__date">2023年6月1日(木)</p>
-        <h1 class="attendance-card__time">08:00</h1>
+        <p id="date" class="attendance-card__date"></p>
+        <h1 id="nowTime" class="attendance-card__time"></h1>
         <form action="/attendance" method="post">
             @csrf
             <button class="btn attendance-card__button">出勤</button>
         </form>
     </section>
 </div>
+<script>
+'use strict'
+    const dateEl = document.getElementById('date');
+    const nowTimeEl = document.getElementById('nowTime');
 
+    function showTime(){
+        let today = new Date();
+        let year = today.getFullYear();
+        let month = today.getMonth() + 1;
+        let day = today.getDate();
+        let weekdays = ['日', '月', '火', '水', '木', '金', '土'];
+        let weekday = weekdays[today.getDay()];
+        let hours = String(today.getHours()).padStart(2, '0');
+        let minutes = String(today.getMinutes()).padStart(2, '0');
+        let formatDate = `${year}年${month}月${day}日(${weekday})`;
+        dateEl.textContent = formatDate;
+        nowTimeEl.textContent = `${hours}:${minutes}`;
+    }
+    showTime();
+    setInterval(showTime, 1000);
+</script>
 @endsection
