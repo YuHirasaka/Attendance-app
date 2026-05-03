4c2d61ff5741b5b740869080eeb793bd0421dd47
yu_hirasaka <yu.hirasaka03@gmail.com>
2026-05-03 22:53:09 +0900

    feat: toastrによるフラッシュメッセージ表示を追加

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -9,22 +9,24 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
     @yield('css')
 </head>
 
 <body>
     @yield('content')
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
     <script>
         toastr.options = {
             "closeButton": true,
             "progressBar": true,
-            "positionClass": "toast-bottom-right",
+            "positionClass": "toast-top-left",
         }
 
         @if(Session::has('flashSuccess'))
-        toastr.success("{{ session('flashSuccess') }}");
+            toastr.success("{{ session('flashSuccess') }}");
         @endif
     </script>
 </body>
