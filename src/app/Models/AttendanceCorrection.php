44af2a06099d6ea67e04ee9f23cf177a366bf934
yu_hirasaka <yu.hirasaka03@gmail.com>
2026-05-03 22:58:40 +0900

    fix: リレーションで使用するモデルのuse文不足を修正

--- src/app/Models/AttendanceCorrection.php
+++ src/app/Models/AttendanceCorrection.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Attendance;
+use App\Models\AttendanceCorrectionBreak;
+use App\Models\User;
 
 class AttendanceCorrection extends Model
 {
