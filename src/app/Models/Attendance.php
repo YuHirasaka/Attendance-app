1dc866935688510b0608fec208fe797ea4d07c46
yu_hirasaka <yu.hirasaka03@gmail.com>
2026-05-03 23:00:01 +0900

    feat: 勤怠ステータス判定ロジックをモデルに追加

--- src/app/Models/Attendance.php
+++ src/app/Models/Attendance.php
@@ -4,10 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\AttendanceCorrection;
+use App\Models\AttendanceBreak;
 
 class Attendance extends Model
 {
     use HasFactory;
+
+    const STATUS_DONE = '退勤済';
+    const STATUS_BREAK = '休憩中';
+    const STATUS_WORKING = '出勤中';
+    const STATUS_NOT_WORKING = '勤務外';
 
     protected $fillable = [
         'work_date',
@@ -28,5 +36,21 @@ class Attendance extends Model
 
     public function Correction(){
         return $this->hasOne(AttendanceCorrection::class);
+    }
+
+    public function status()
+    {
+        if($this->check_out){
+            return self::STATUS_DONE;
+        } elseif ($this->breaks()
+                    ->whereNotNull('break_start')
+                    ->whereNull('break_end')
+                    ->exists()) {
+            return self::STATUS_BREAK;
+        } elseif ($this->check_in) {
+            return self::STATUS_WORKING;
+        } else {
+            return self::STATUS_NOT_WORKING;
+        }
     }
 }
