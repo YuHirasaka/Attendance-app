3c82b21ba72ae3a2d7578918d554f3386a7b57ae
yu_hirasaka <yu.hirasaka03@gmail.com>
2026-05-10 16:41:33 +0900

    refactor: 勤怠日時カラムのキャスト設定を追加

--- src/app/Models/Attendance.php
+++ src/app/Models/Attendance.php
@@ -24,6 +24,12 @@ class Attendance extends Model
         'note'
     ];
 
+    protected $casts = [
+        'work_date' => 'date',
+        'check_in' => 'datetime',
+        'check_out' => 'datetime',
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
