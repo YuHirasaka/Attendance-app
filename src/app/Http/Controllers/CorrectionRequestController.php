680b610784e71d6666edfda5d97785b41228d220
yu_hirasaka <yu.hirasaka03@gmail.com>
2026-05-21 09:55:32 +0900

    feat: 修正申請用コントローラを追加

---
+++ src/app/Http/Controllers/CorrectionRequestController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\AttendanceCorrection;
+use Illuminate\Support\Facades\Auth;
+
+class CorrectionRequestController extends Controller
+{
+    public function index(Request $request)
+    {
+        $page = $request->page;
+        $user = Auth::user();
+
+        if ($page === 'pending') {
+            $attendanceCorrections = AttendanceCorrection::where('status', 'pending')
+            ->whereHas('attendance', function ($query) use ($user) {
+                $query->where('user_id', $user->id);
+            })
+            ->get();
+        } else {
+            $attendanceCorrections = AttendanceCorrection::where('status','approved')
+            ->whereHas('attendance', function ($query) use ($user) {
+                $query->where('user_id', $user->id);
+            })
+            ->get();
+        }
+
+        return view('correction.index', compact('attendanceCorrections'));
+    }
+
+    public function show($attendanceCorrection_id)
+    {
+        $correction = AttendanceCorrection::with('attendance.breaks')->findOrFail($attendanceCorrection_id);
+
+        $attendance = $correction->attendance;
+
+        return view('attendance.edit', [
+            'attendance' => $attendance,
+            'correction' => $correction,
+            'isReadonly' => true,
+        ]);
+    }
+}
